06f1927c473c1ebe5cdd696ffb046190ac9e0f41
etikivishwas <etikivishwas47@gmail.com>
2025-08-24 20:11:46 +0530

    worker backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,12 @@ app.use(express.json());
 // Routes
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
+
+const workerRoutes = require("./routes/workers");
+app.use("/api/workers", workerRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 // Start Server
 const PORT = process.env.PORT || 5000;
